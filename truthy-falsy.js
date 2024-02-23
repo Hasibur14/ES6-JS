a527bd60359c0213f8cda150c138ffee1d309608
hasibur14 <hasib141415@gmail.com>
2024-02-23 23:18:51 +0600

    added more js problem

---
+++ truthy-falsy.js
@@ -0,0 +1,13 @@
+const x = " ";
+if(x){
+    console.log('value x is a truthy')
+}
+else{
+    console.log('value x is a falsy')
+};
+
+// x = 10 ---> truthy
+//x = 0 -----> falsy
+//x = -5 ----> truthy
+// x = " "---->falsy
+// x = '0'------> truthy
